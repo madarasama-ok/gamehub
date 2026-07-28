4847ab9c1e682e50432b46a05d399298921ed40b
Ubuntu <ubuntu@ip-172-31-39-87.us-east-2.compute.internal>
2026-07-28 07:50:32 +0000

    fix admin bearer auth

--- artifacts/gamehub/src/main.tsx
+++ artifacts/gamehub/src/main.tsx
@@ -1,7 +1,13 @@
 import { createRoot } from 'react-dom/client';
 
+import { setAuthTokenGetter } from "@workspace/api-client-react";
+
 import App from './App';
 
 import './index.css';
+
+setAuthTokenGetter(() => {
+  return localStorage.getItem("admin_key");
+});
 
 createRoot(document.getElementById('root')!).render(<App />);
